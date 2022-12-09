a8f9259b6255dc97e67b9c4e9d489d23a0fc0345
RoggemansC <Cara.Roggemans@acolad.com>
2022-12-09 14:32:32 +0100

    search users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -15,19 +15,23 @@ export const GithubProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
     
-    //get initial users (testing purposes)
-    const fetchUsers = async () => {
+    //get search results
+    const searchUsers = async (text) => {
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`,  {
+        const params = new URLSearchParams({
+            q: text
+        })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`,  {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
     })
-    const data = await response.json()
+    const {items} = await response.json()
         
     dispatch({
         type: 'GET_USERS',
-        payload: data
+        payload: items
     })
     }
 
@@ -35,7 +39,7 @@ export const GithubProvider = ({children}) => {
     const setLoading = () => dispatch({
 type: 'SET_LOADING'})
     return <GithubContext.Provider value={{
-        users:state.users, loading:state.loading, fetchUsers
+        users:state.users, loading:state.loading, searchUsers
         }}>
         {children}
     </GithubContext.Provider>
