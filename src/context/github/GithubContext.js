906faa657f77026be19774d9c7fc9bdb2da5f36b
RoggemansC <Cara.Roggemans@acolad.com>
2022-12-09 14:38:18 +0100

    clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -35,11 +35,19 @@ export const GithubProvider = ({children}) => {
     })
     }
 
+    //clear users from state
+    const clearUsers = () => dispatch({
+        type: 'CLEAR_USERS'
+    })
+
     //set loading
     const setLoading = () => dispatch({
 type: 'SET_LOADING'})
     return <GithubContext.Provider value={{
-        users:state.users, loading:state.loading, searchUsers
+        users:state.users, 
+        loading:state.loading, 
+        searchUsers,
+        clearUsers,
         }}>
         {children}
     </GithubContext.Provider>
