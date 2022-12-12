2e85b4dff63f6cc2346f93eddbe2fc4137aa0d0f
RoggemansC <Cara.Roggemans@acolad.com>
2022-12-12 11:31:22 +0100

    cleanup actions & add axios

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -5,24 +5,22 @@ import { useParams, Link } from 'react-router-dom'
 import Spinner from '../layouts/Spinner'
 import RepoList from '../repos/RepoList'
 import GithubContext from '../../context/github/GithubContext'
-import { getUser, getUserRepos } from '../../context/github/GithubActions'
+import { getUserAndRepos } from '../../context/github/GithubActions'
+
 function User() {
-    const {user, loading, repos, dispatch} = useContext(GithubContext)
+  const { user, loading, repos, dispatch } = useContext(GithubContext)
 
-    const params = useParams()
+  const params = useParams()
 
+  useEffect(() => {
+    dispatch({ type: 'SET_LOADING' })
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login)
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData })
+    }
 
-    useEffect(() => {
-      dispatch({tupe: 'SET_LOADING'})
-       const getUserData = async() => {
-          const userData = await getUser(params.login)
-          dispatch({type: 'GET_USER', payload: userData})
-
-          const userRepoData = await getUserRepos(params.login)
-          dispatch({type: 'GET_REPOS', payload: userRepoData})
-       }
-       getUserData()
-    },[])
+    getUserData()
+  }, [dispatch, params.login])
 
     const {
         name,
