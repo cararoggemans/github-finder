8c5997594b3c563f8697cd127f2cb94bfeeafd78
RoggemansC <Cara.Roggemans@acolad.com>
2022-12-12 10:52:18 +0100

    move searchUsers to actions file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -18,26 +18,6 @@ export const GithubProvider = ({children}) => {
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
-    
-    //get search results
-    const searchUsers = async (text) => {
-        setLoading()
-        const params = new URLSearchParams({
-            q: text
-        })
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`,  {
-                headers: {
-                    Authorization: `token ${GITHUB_TOKEN}`
-                }
-        })
-        const {items} = await response.json()
-            
-        dispatch({
-            type: 'GET_USERS',
-            payload: items
-        })
-    }
 
     //clear users from state
     const clearUsers = () => dispatch({
@@ -93,14 +73,11 @@ type: 'SET_LOADING'})
 
 
     return <GithubContext.Provider value={{
-        users:state.users, 
-        loading:state.loading, 
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
         clearUsers,
         getUser,
         getUserRepos,
+        dispatch,
         }}>
         {children}
     </GithubContext.Provider>
